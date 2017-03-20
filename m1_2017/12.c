8cd537c93f81e82434525b3832c0dd510a32f9a3
NikolaevDenisPolyt <den11nik@yandex.ru>
2017-03-20 23:14:23 +0300

    Update 12.c

--- m1_2017/12.c
+++ m1_2017/12.c
@@ -3,10 +3,9 @@
 #include <locale.h>
 int main()
 {setlocale(LC_CTYPE, "Russian");
-int m,i,b[1000],j=0,k=0,x;
+int i,b[1000];
 char c[1000];
 gets(c);
-m=-1;
 for(i=0;c[i]!='\0';i++)
 {
 b[i]=c[i];
