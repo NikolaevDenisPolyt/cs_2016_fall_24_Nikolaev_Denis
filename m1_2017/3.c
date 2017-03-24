cb6c94f25d962032a08f814b07b8edbe0fb9fc8b
NikolaevDenisPolyt <den11nik@yandex.ru>
2017-03-24 22:25:04 +0300

    Update 3.c

--- m1_2017/3.c
+++ m1_2017/3.c
@@ -4,8 +4,10 @@ int main() {
 
     setlocale(LC_CTYPE, "Russian");
 	float x,y,a,b,c,d;
-	printf("Введите числа в формате a+bi,x+yi\n ");
-	scanf("%f+%fi,%f+%fi", &x, &y, &a, &b);
+	printf("Введите число в формате a+bi\n ");
+	scanf("%f+%fi", &x, &y);
+	printf("Введите число в формате a+bi\n ");
+	scanf("%f+%fi", &a, &b);
 	c = (x*x*a+x*a*a+x*b*b+a*y*y)/((x+a)*(x+a)+(y+b)*(y+b));
 	d = (x*x*b+a*a*y+y*y*b+y*b*b)/((x+a)*(x+a)+(y+b)*(y+b));
 	printf("(%.3f)+(%.3fi)", c, d);
