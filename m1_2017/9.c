5356a4d3a4d2c05b419451d708c48c3b4e47401b
NikolaevDenisPolyt <den11nik@yandex.ru>
2017-03-25 10:13:02 +0300

    Update 9.c

--- m1_2017/9.c
+++ m1_2017/9.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
-#include <locale.h>
-int fib(int x1)
-{
-    if (x1==0||x1==1)
-    {
-        return x1;
-    }
-    else
-    {
-        return fib(x1-1)+fib(x1-2);
-    }
-
-
-}
-int main()
-{
-    setlocale(LC_CTYPE, "Russian");
-    int x1;
-    printf("Введите номер : ");
-    scanf("%i", &x1);
-    printf("%i:число Фибоначчи номер %i" , fib(x1),x1);
-    return 0;
-
+#include <stdio.h> 
+#include <locale.h> 
+int fib(int x1) 
+{ 
+ if (x1==0||x1==1) 
+ { 
+ return x1; 
+ } 
+ else 
+ { 
+ return fib(x1-1)+fib(x1-2); 
+ } 
+ 
+ 
+} 
+int main() 
+{ 
+ setlocale(LC_CTYPE, "Russian"); 
+ int x1, i; 
+ printf("Введите номер : "); 
+ scanf("%i", &x1); 
+ for (i=1;i<x1+1;++i){ 
+ printf("%i:%i\n", i, fib(i)); 
+ } 
+ return 0; 
+ 
 }
